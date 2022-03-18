acb65de18f83db40059c8acb95c2f238ad7fc63f
devdanielsan <devdanielsanches@gmail.com>
2022-03-18 15:38:54 -0300

    rest testado

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -3,6 +3,8 @@ package com.nelioalves.cursomc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 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
+
 @SpringBootApplication
 public class CursomcApplication {
 

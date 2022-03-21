956b8e9ec07b5685748c8fb1cc63acc27cf45107
devdanielsan <devdanielsanches@gmail.com>
2022-03-21 13:56:54 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/nelioalves/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.nelioalves.cursomc.domain.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+	//void saveAll(List<Categoria> asList);
 
 }
